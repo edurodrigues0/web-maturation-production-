1a6af25ca53372f350ce67d443e2f9e5ee5eff18
edurodrigues0 <edusacramento9@gmail.com>
2024-06-10 22:01:14 -0300

    add building page

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Dashboard } from '../pages/Dashboard'
 import { Colaborators } from '../pages/Colaborators'
 import { NotFound } from '../pages/NotFound'
+import { AuthProduction } from '../pages/AuthProduction'
 
 
 export function AppRoutes() {
@@ -9,6 +10,8 @@ export function AppRoutes() {
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/colaboradores" element={<Colaborators />} />
+      <Route path="/producao" element={<AuthProduction />} />
+      <Route path="/tarefas" element={<AuthProduction />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
